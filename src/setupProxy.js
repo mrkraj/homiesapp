94a84b7d2230da7c9142a922abd28350d2633f5e
LAPTOP-BL5AR6FO\mrkra <mrkrajkumar212@gmail.com>
2020-12-20 00:56:55 -0500

    proxy updated

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/homies',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: 'http://homies-env.eba-jdsdvmvp.us-west-2.elasticbeanstalk.com',
       changeOrigin: true,
     })
   );
